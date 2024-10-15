c03123a7ddf570d11f9f90166cf2fbcd95ff127e
bsch0111 <bsch0111@naver.com>
2024-10-16 01:26:28 +0900

    feat : make_api 추가

---
+++ make_com_api.py
@@ -0,0 +1,22 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MAKE_COM_WEBHOOK_URL = os.getenv('MAKE_COM_WEBHOOK_URL')
+
+class MakeComApi:
+    def __init__(self):
+        self.url = MAKE_COM_WEBHOOK_URL
+        self.headers = {
+            'Content-Type': 'application/json'
+        }
+
+    def send_notification(self, data: dict):
+
+        response = requests.post(self.url, json=data, headers=self.headers)
+        if response.status_code == 200:
+            print("Notification sent successfully")
+        else:
+            print(f"Failed to send notification. Status code: {response.status_code}")
