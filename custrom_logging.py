bf8909a41d509a21d0b254457ea8601cb613710f
bsch0111 <bsch0111@naver.com>
2024-10-27 16:15:58 +0900

    feat: release note 추가

--- custrom_logging.py
+++ custrom_logging.py
@@ -1,8 +1,10 @@
 import logging
 
+
 def set_logging(level=logging.INFO):
-    logging.basicConfig(                                                      
-        level=logging.INFO, format="%(name)s - %(asctime)s - %(levelname)s - %(message)s"
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(name)s - %(asctime)s - %(levelname)s - %(message)s",
     )
     logger = logging.getLogger(__name__)
     logger.setLevel(level)
