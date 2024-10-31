8053c6fa09ffb912e3f91177999e6f9a9a611f60
bsch0111 <bsch0111@naver.com>
2024-11-01 01:12:31 +0900

    test: black test 파일 추가

---
+++ black_test.py
@@ -0,0 +1,20 @@
+print("aasaad")
+
+
+def test_black():
+    print("aasaad")
+    print("aasaad")
+    print("aasaad")
+
+
+
+    test = {
+        "test": "test"
+    }
+
+    for i in range(1,10):
+        print(f"{i}")
+
+
+if __name__ == "__main__":
+    test_black()
